2a7384a2f2faa9662cb0fd565f92dcfc01611ac3
Steve Elliott <steve@tegud.net>
2014-03-01 20:13:43 +0000

    Form is now fully dynamic, loading from json files

--- index.js
+++ index.js
@@ -7,6 +7,7 @@ var handlebars = hbs.handlebars;
 var LeanKitClient = require('leankit-client');
 var fs = require('fs');
 var _ = require('lodash');
+var BuildFormViewModel = require('./lib/FormViewModelFactory');
 
 var server = function() {
     var httpServer;
@@ -54,133 +55,20 @@ var server = function() {
         });
     });
 
+    app.get('/:team/:form', function(req, res) {
+        fs.readFile(__dirname + '/teams/' + req.params.team + '/' + req.params.form + '.json', { encoding: 'utf-8' }, function(err, fileContents) {
+            if(err) {
+                res.end('Form or Team not found');
+                return;
+            }
+
+            var data = JSON.parse(fileContents);
+            res.render('index.hbs', BuildFormViewModel(data));
+        });
+    });
+
     app.use('/', function(req, res) {
-        function createViewModel(formInfo) {
-            _.each(formInfo.sections, function(section) {
-                _.each(section.fields, function(field) {
-                    field.fieldCssClass = 'request-item';
-                    field.name = field.name || field.fillpoint || field.label.replace(/ /g, '-').toLowerCase();
-
-                    if(field.options) {
-                        field.isMultiChoice = true;
-                    }
-                    else if(field.type === 'description') {
-                        field.isTextArea = true;
-                        field.fieldCssClass += ' request-details';
-                    }
-                    else {
-                        field.isTextField = true;
-                        field.fieldCssClass += ' single-line';
-                    }
-
-                    if(field.size === 'small') {
-                        field.fieldCssClass += ' small';   
-                    }
-                });
-            });
-
-            return formInfo;
-        }
-
-        var data  = {
-            title: 'LateRooms BAU Ticket Requests',
-            summary: 'In order for the business to effectively prioritise the IT backlog all requests must be made through the completion of this form. The more detail you are able to provide (particularly in terms of defining the business benefit) the greater opportunity that your ticket will be prioritised and worked upon.',
-            sections: [
-                {
-                    fields: [
-                        {
-                            label: 'Your Name',
-                            type: 'tag'
-                        },
-                        {
-                            label: 'Department',
-                            type: 'tag'
-                        },
-                        {
-                            label: 'Name of Ticket',
-                            type: 'title'
-                        }
-                    ]
-                },
-                {
-                    fields: [
-                        {
-                            label: 'Details of Ticket / Summary of Issue',
-                            type: 'description',
-                            fillpoint: 'summary'
-                        },
-                        {
-                            label: 'Requirements',
-                            type: 'description',
-                            fillpoint: 'requirements'
-                        }
-                    ]
-                },
-                {
-                    /*
-                    <li>
-                        <label>Identify if the change will affect more than one page</label>
-                        <div class="request-item" data-field-type="multi-choice">
-                            <label><input type="radio" name="MoreThanOnePage" value="Affects more than one page" /> Yes</label>
-                            <label><input type="radio" name="MoreThanOnePage" value="" checked /> No</label>
-                        </div>
-                    </li>
-                    <li>
-                        <label>Identify the specific expected behaviour</label>
-                    </li>
-                    <li>
-                        <label>Identify the specific constraints</label>
-                    </li>
-                    <li>
-                        <label>Identify if a new text will be introduced</label>
-                    </li>
-                    <li>
-                        <label>Translation of new text </label>
-                    </li>
-                    <li>
-                        <label>Identify if the artwork is needed and provided</label>
-                    </li>
-                    */
-                    fields: [
-                        {
-                            label: 'Which site / sites will the change be made to?',
-                            name: 'Sites',
-                            type: 'tag',
-                            options: [
-                                { value: 'LR.com' },
-                                { value: 'LR.com Mobile' },
-                                { value: 'AR.com' },
-                                { value: 'AR.com Mobile' },
-                            ]
-                        },
-                        { label: '',  }
-                    ]
-                }, 
-                {
-                    fields: [
-                        {
-                            label: 'Background / Reason for change',
-                            type: 'description',
-                            fillpoint: 'background',
-                            size: 'small'
-                        },
-                        {
-                            label: 'Business Rationale / Impact',
-                            type: 'description',
-                            fillpoint: 'businessRationale'
-                        },
-                        {
-                            label: 'How will success be measured?',
-                            type: 'description',
-                            fillpoint: 'successMeasurement',
-                            size: 'small'
-                        }
-                    ]
-                }
-            ]
-        };
-
-        res.render('index.hbs', createViewModel(data));
+        res.end('Welcome to ticket wizard');
     });
 
     return {
