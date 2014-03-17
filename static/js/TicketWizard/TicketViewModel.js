916cf35c2c843bdff01a44ee39063a04e3771e28
Steve Elliott <steve@tegud.net>
2014-03-17 23:44:35 +0000

    Table builder passes, much refactoring to be done

--- static/js/TicketWizard/TicketViewModel.js
+++ static/js/TicketWizard/TicketViewModel.js
@@ -3,6 +3,37 @@
 
     window['TW'] = window['TW'] || {};
 
+    var componentBuilders = {
+        'field': function buildFromField(field) {
+            return field.val().replace(/\r?\n/g, "<br />");
+        },
+        'table': function buildFromTable(item) {
+            var rows = [];
+            var headerRows = $('.header-row:not(.template-row)', item);
+            var rowElements = headerRows.map(function() {
+                return $('.row-' + $(this).data('rowUid') + ':not(.header-row)');
+            });
+
+            rowElements.each(function() {
+                var columns = [];
+
+                this.each(function() {
+                    var inputField = $('.table-input', this);
+
+                    columns.push({ name: inputField.data('fillpoint'), value: componentBuilders['field'](inputField)} );
+                });
+
+                rows.push({
+                    columns: columns
+                });
+            });
+
+            return {
+                rows: rows
+            };
+        }
+    };
+
     TW.TicketViewModel = function(rootElement) {
         return {
             build: function() {
@@ -12,12 +43,18 @@
                 $('.request-item', rootElement).each(function() {
                     var item = $(this);
                     var appendTo = item.data('appendTo');
+                    var fillpoint = item.data('fillpoint');
 
-                    if(appendTo === 'title') {
-                        titles.push(item.val());
+                    if(this.tagName.toLowerCase() === 'table') {
+                        description[fillpoint] = componentBuilders['table'](item);
                     }
                     else {
-                        description[item.data('fillpoint')] = item.val().replace(/\r?\n/g, "<br />");
+                        if(appendTo === 'title') {
+                            titles.push(item.val());
+                        }
+                        else {
+                            description[fillpoint] = componentBuilders['field'](item);
+                        }
                     }
                 });
 
--- test/client/tests/TicketViewModel.js
+++ test/client/tests/TicketViewModel.js
@@ -47,21 +47,66 @@
         describe('Table', function() {
             beforeEach(function() {
                 $('body').append('<form id="test-fixture">' +
-                    '<input type="text" class="request-item" id="field-one" data-append-to="title" value="testValue" />' +
-                    '<textarea class="request-item" id="field-two" data-append-to="title">anotherTestValue</textarea>' +
-                    '<input type="text" class="request-item" id="field-three" data-append-to="description" data-fillpoint="two" value="secondTestValue" />' +
-                    '<textarea class="request-item" id="field-four" data-append-to="description" data-fillpoint="three">thirdTestValue</textarea>' +
+                    '<table class="request-item" data-fillpoint="Requirements">' +
+                        '<tr class="header-row" data-row-uid="0">' +
+                            '<th colspan="2">' +
+                                '<div>Requirement #<span class="row-index">1</span></div>' +
+                                '<a class="btn btn-danger delete-row-button" role="button"><span class="glyphicon glyphicon-trash requirement-delete"></span></a>' +
+                            '</th>' +
+                        '</tr>' +
+                        '<tr class="field-row row-0">' +
+                            '<th>As a</th>' +
+                            '<td><input type="text" class="table-input" data-fillpoint="As a" value="User of a website"/></td></tr>' +
+                        '<tr class="field-row row-0">' +
+                            '<th>I Would like</th>' +
+                            '<td class="textarea-cell"><textarea class="table-input table-textarea" data-fillpoint="I would like">to click a\r\nbutton</textarea></td>' +
+                        '</tr>' +
+                        '<tr class="field-row row-0 last-row">' +
+                            '<th>So that</th>' +
+                            '<td class="textarea-cell"><textarea class="table-input table-textarea" data-fillpoint="So that">Money is made</textarea></td>' +
+                        '</tr>' +
+                        '<tr class="template-row header-row">' +
+                            '<th colspan="2" class="table-header">' +
+                                '<div class="table-header-text">Requirement #<span class="row-index">2</span></div>' +
+                                '<a class="btn btn-danger delete-row-button" role="button"><span class="glyphicon glyphicon-trash requirement-delete"></span></a>' +
+                            '</th>' +
+                        '</tr>' +
+                        '<tr class="template-row field-row ">' +
+                            '<th>As a</th>' +
+                            '<td><input type="text" class="table-input" data-fillpoint="As a" /></td>' +
+                        '</tr>' +
+                        '<tr class="template-row field-row ">' +
+                            '<th>I Would like</th>' +
+                            '<td class="textarea-cell"><textarea class="table-input table-textarea"></textarea></td>' +
+                        '</tr>' +
+                        '<tr class="template-row field-row  last-row">' +
+                        '<th>So that</th>' +
+                            '<td class="textarea-cell"><textarea class="table-input table-textarea" data-fillpoint="So that"></textarea></td>' +
+                        '</tr>' +
+                        '<tr>' +
+                            '<td colspan="2" class="new-row"><a class="btn btn-success add-row-button" role="button"><span class="glyphicon glyphicon-plus"></span> Add Requirement</a></td>' +
+                        '</tr>' +
+                    '</table>' +
                     '</form>');
             });
 
-            it('sets replaces new line characters with <br /> tags in description', function() {
+            it('sets table description model', function() {
                 var viewModelFactory = new TW.TicketViewModel($('#test-fixture'));
-
-                $('#field-four').val('one\r\ntwo');
-
                 var viewModel = viewModelFactory.build();
 
-                expect(viewModel.description.three).to.eql('one<br />two');
+                expect(viewModel.description).to.eql({
+                    Requirements: {
+                        rows: [
+                            {
+                                columns: [
+                                    { name: 'As a', value: 'User of a website' },
+                                    { name: 'I would like', value: 'to click a<br />button' },
+                                    { name: 'So that', value: 'Money is made' }
+                                ]
+                            }
+                        ]
+                    }
+                });
             });
         });
     });
