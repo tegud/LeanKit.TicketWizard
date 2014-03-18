a41eec6873cda98e9b2262d5f1825a5e4e4758e2
Steve Elliott <steve@tegud.net>
2014-03-18 13:57:56 +0000

    Refactoring away some duplication

--- static/js/TicketWizard/TicketViewModel.js
+++ static/js/TicketWizard/TicketViewModel.js
@@ -34,34 +34,51 @@
         }
     };
 
+    function Model() {
+        var model = {
+            title: [],
+            description: {}
+        };
+
+        return {
+            title: function(value) {
+                model.title.push(value);
+            },
+            description: function(value, fillpoint) {
+                model.description[fillpoint] = value;
+            },
+            get: function() {
+                var clone = $.extend(true, {}, model);
+                clone.title = clone.title.join(' ');
+                return clone;
+            }
+        };
+    }
+
+    function getComponentFromTagName(tagName) {
+        if(tagName.toLowerCase() === 'table') {
+            return 'table';
+        }
+
+        return 'field';
+    }
+
     TW.TicketViewModel = function(rootElement) {
         return {
             build: function() {
-                var titles = [];
-                var description = {};
+                var model = new Model();
 
                 $('.request-item', rootElement).each(function() {
                     var item = $(this);
-                    var appendTo = item.data('appendTo');
+                    var appendTo = item.data('appendTo') || 'description';
                     var fillpoint = item.data('fillpoint');
+                    var component = getComponentFromTagName(this.tagName);
+                    var value = componentBuilders[component](item);
 
-                    if(this.tagName.toLowerCase() === 'table') {
-                        description[fillpoint] = componentBuilders['table'](item);
-                    }
-                    else {
-                        if(appendTo === 'title') {
-                            titles.push(item.val());
-                        }
-                        else {
-                            description[fillpoint] = componentBuilders['field'](item);
-                        }
-                    }
+                    model[appendTo](value, fillpoint);
                 });
 
-                return {
-                    title: titles.join(' '),
-                    description: description
-                };
+                return model.get();
             }
         };
     };
